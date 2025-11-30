6a4c4b6e0058ba91ec0d562240b81cfe85b640ea
myspring <myspring>
2025-11-30 19:39:36 +0800

    add files

---
+++ DeZero/__init__.py
@@ -0,0 +1,28 @@
+from .config import Config
+from .variable import Variable
+from .function import (
+    Function,
+    Square,
+    Exp,
+    Mul,
+    square,
+    exp,
+    mul,
+    as_array,
+    as_variable,
+)
+
+__all__ = [
+    "Config",
+    "Variable",
+    "Function",
+    "Square",
+    "Exp",
+    "Mul",
+    "square",
+    "exp",
+    "mul",
+    "as_array",
+    "as_variable",
+]
+
